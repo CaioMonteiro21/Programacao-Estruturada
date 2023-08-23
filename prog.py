594109a2618539fbeb6e993d8ba8a78ac926386e
CaioMonteiro21 <142441252+CaioMonteiro21@users.noreply.github.com>
2023-08-23 10:30:52 -0300

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,21 @@
+a = float(input('digite o valor a= '))
+b = float(input('digite o valor b= '))
+c = float(input('digite o valor c= '))
+              
+delta = (b**2) - (4*a*c)
+print('delta=', delta)
+raizdedelta=delta*0.5
+print('raiz de delta= ', raizdedelta)
+x1=(-b+raizdedelta) / (2*a)
+print('x1= ', x1)
+x2=(-b-raizdedelta) / (2*a)
+print('x2=', x2)
+
+
+
+
+
+
+
+
+
