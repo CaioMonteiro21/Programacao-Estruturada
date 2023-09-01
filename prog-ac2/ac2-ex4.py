78af257ccafb5a58e8d9522d2e21022d48cdf201
Caio Monteiro <142441252+CaioMonteiro21@users.noreply.github.com>
2023-09-01 19:26:18 -0300

    Add files via upload

---
+++ prog-ac2/ac2-ex4.py
@@ -0,0 +1,18 @@
+#exercicio 4
+
+ano = int(input('digite o ano'))
+if ano % 100 != 0 and ano % 4 == 0 or ano % 400==0:
+    print('é um ano bissexto')
+else:
+    print('não é bissexto')
+    
+
+
+
+
+
+
+
+
+
+
